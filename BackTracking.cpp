50b5883351e2b6549498654c44e2a41bd9e40915
DESKTOP-KIP6B0M\Hung <wowmrt1999@gmail.com>
2018-05-13 20:59:36 +0700

    done implementing n Queen problem yay!

--- BackTracking.cpp
+++ BackTracking.cpp
@@ -1,17 +1,20 @@
 #include "iostream"
 using namespace std;
 
-void nQueen(int **a, int column);
+bool nQueen(int **board, int column, int boardSize);
+void printBoard(int **board, int boardSize);
+void constructor(int **&board, int boardSize);
+bool isSafe(int **board, int currentRow, int currentColumn, int boardSize);
+int findColumnQueen(int **board, int currentColumn, int boardSize);
+int main(){
 
-int main() {
+  int **board;
+  int boardSize=4;
+  constructor(board,boardSize);
+  //printBoard(board,boardSize);
+  if((nQueen(board,0,boardSize)))
+    printBoard(board,boardSize);
 
-  int **a;
-  int n=4;
-  a = new int*(nothrow)[n];
-  for(int i=0;i<n;i++)
-  {
-    a[i]=new int(nothrow)[n];
-  }
   return 0;
 }
 
@@ -23,15 +26,20 @@ bool nQueen(int **board, int column, int boardSize)
   {
     for (int row = 0; row < boardSize; row++ ) {// find a safe position for the queen at currentColumn
       /* code */
+
       if(isSafe(board,row,column,boardSize))//pick a safe position for current queen
       {
         board[row][column] = 1;
+
+        printBoard(board,boardSize);
+        cout << '\n';
 
         if(nQueen(board,column+1,boardSize)) // go to next column, if false here mean not safe pos ava in the next col
           return true;
 
         //backtrack to current and delete current queen and find another pos
-        board[row][column] = 0; 
+        board[row][column] = 0;
+
       }
     }
 
@@ -42,11 +50,12 @@ bool nQueen(int **board, int column, int boardSize)
 bool isSafe(int **board, int currentRow, int currentColumn, int boardSize)
 {
   int queenColumn = 0, queenRow = 0;
+  if(currentColumn == 0 ) return true;
   for(queenColumn = 0; queenColumn < currentColumn; queenColumn++)
   {
     queenRow = findColumnQueen(board, queenColumn, boardSize);
     if(currentRow == queenRow ) return false;
-    for(int queenRange = 1; (queenColumn + queenRange) <= currentColumn; queenColumn++)
+    for(int queenRange = 1; (queenColumn + queenRange) <= currentColumn; queenRange++)
     {
       //Queen attack range
       bool aboveDiag = currentRow==(queenRow - queenRange) && currentColumn==(queenColumn+queenRange);
@@ -85,9 +94,8 @@ bool isSafe(int **board, int currentRow, int currentColumn, int boardSize)
       }
 
     }
-
-    return true;
   }
+  return true;
 }
 
 int findColumnQueen(int **board, int currentColumn, int boardSize)
@@ -97,4 +105,25 @@ int findColumnQueen(int **board, int currentColumn, int boardSize)
     if(board[currentRow][currentColumn]==1) return currentRow;
   }
   return -1;
+}
+
+void printBoard(int **board, int boardSize){
+  for(int i = 0; i< boardSize; i++){
+    for (int j=0; j < boardSize; j++) {
+      cout << board[i][j] << "\t";
+    }
+    cout << "\n";
+  }
+}
+
+void constructor(int **&board, int boardSize){
+  board = new int*[boardSize];
+  for(int i=0;i<boardSize;i++)
+  {
+    board[i]=new int[boardSize];
+    for(int j=0;j<boardSize;j++)
+    {
+      board[i][j]=0;
+    }
+  }
 }
